a3d2de840628a776d10d37a287167653088c9cf7
Freek Boes <r0984517@student.thomasmore.be>
2025-07-29 16:33:47 +0200

    homepagina aangepast

---
+++ V3/script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+// Zorg dat het menu gesloten is bij pagina laden
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenu = document.getElementById("mobile-menu");
+  if (mobileMenu) {
+    mobileMenu.style.display = "none";
+  }
+});
